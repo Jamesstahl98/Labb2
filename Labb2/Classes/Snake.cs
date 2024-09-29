f9e6c164a71135f549a9b4c58300bd0363cb3a4a
Jamesstahl98 <Jamesstahl556@gmail.com>
2024-09-29 22:23:40 +0200

    Update Snake.cs
    Snakes now move away from player

--- Labb2/Classes/Snake.cs
+++ Labb2/Classes/Snake.cs
@@ -1,4 +1,6 @@
-﻿public class Snake : Enemy
+﻿using System.Xml.Linq;
+
+public class Snake : Enemy
 {
     public Snake(Position pos, char c, ConsoleColor color) : base(pos, c, color)
     {
@@ -10,6 +12,35 @@
 
     public override void Update()
     {
-        //if(math.abs(player.x, snake.x) < 3)
+        foreach(LevelElement element in LevelData.Elements)
+        {
+            if(element is Player)
+            {
+                int posXDiff = Math.Abs(PosX - element.PosX);
+                int posYDiff = Math.Abs(PosY - element.PosY);
+
+                if ((posXDiff * posXDiff) + (posYDiff * posYDiff) < 4)
+                {
+                    var newPos = new Position(PosX, PosY);
+
+                    if (posXDiff>posYDiff)
+                    {
+                        newPos.X += Math.Sign(PosX - element.PosX);
+                    }
+                    else
+                    {
+                        newPos.Y += Math.Sign(PosY - element.PosY);
+                    }
+                    if (IsFreeSpace(newPos))
+                    {
+                        Console.SetCursorPosition(PosX, PosY);
+                        Console.Write(" ");
+                        PosX = newPos.X;
+                        PosY = newPos.Y;
+                    }
+                    Draw();
+                }
+            }
+        }
     }
 }
