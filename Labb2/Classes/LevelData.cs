3934cd2047cc5ededae2e23273fb4e5e94ca4a58
Jamesstahl98 <92599615+Jamesstahl98@users.noreply.github.com>
2024-10-02 15:15:14 +0200

    Update LevelData.cs
    Changed text file  char reader from if-statement to switch

--- Labb2/Classes/LevelData.cs
+++ Labb2/Classes/LevelData.cs
@@ -18,52 +18,40 @@ public static class LevelData
                 for (int j = 0; j <= lines[i].Length+1; j++)
                 {
                     int charUnicode = reader.Read();
-                    if (charUnicode == 33)
+                    switch(charUnicode)
                     {
-                        Potion potion = new Potion(new Position(j, i + 3), '!', ConsoleColor.DarkGreen);
-                        _elements.Add(potion);
-                    }
-
-                    if (charUnicode == 35)
-                    {
-                        Wall wall = new Wall(new Position(j, i + 3), '#', ConsoleColor.Gray);
-                        _elements.Add(wall);
-                    }
-
-                    else if(charUnicode == 64)
-                    {
-                        player = new Player(new Position(j, i + 3), '@', ConsoleColor.Yellow);
-                        _elements.Add(player);
-                    }
-
-                    else if (charUnicode == 97)
-                    {
-                        Armor armor = new Armor(new Position(j, i + 3), 'a', ConsoleColor.DarkYellow);
-                        _elements.Add(armor);
-                    }
-
-                    else if (charUnicode == 108)
-                    {
-                         Sword sword = new Sword(new Position(j, i + 3), 'l', ConsoleColor.DarkYellow);
-                        _elements.Add(sword);
-                    }
-
-                    else if (charUnicode == 114)
-                    {
-                        Rat rat = new Rat(new Position(j, i + 3), 'r', ConsoleColor.Red);
-                        _elements.Add(rat);
-                    }
-
-                    else if (charUnicode == 115)
-                    {
-                        Snake snake = new Snake(new Position(j, i + 3), 's', ConsoleColor.Green);
-                        _elements.Add(snake);
-                    }
-
-                    else if (charUnicode == 116)
-                    {
-                        Troll troll = new Troll(new Position(j, i + 3), 't', ConsoleColor.DarkCyan);
-                        _elements.Add(troll);
+                        case 33:
+                            Potion potion = new Potion(new Position(j, i + 3), '!', ConsoleColor.DarkGreen);
+                            _elements.Add(potion);
+                            break;
+                        case 35:
+                            Wall wall = new Wall(new Position(j, i + 3), '#', ConsoleColor.Gray);
+                            _elements.Add(wall);
+                            break;
+                        case 64:
+                            player = new Player(new Position(j, i + 3), '@', ConsoleColor.Yellow);
+                            _elements.Add(player);
+                            break;
+                        case 97:
+                            Armor armor = new Armor(new Position(j, i + 3), 'a', ConsoleColor.DarkYellow);
+                            _elements.Add(armor);
+                            break;
+                        case 108:
+                            Sword sword = new Sword(new Position(j, i + 3), 'l', ConsoleColor.DarkYellow);
+                            _elements.Add(sword);
+                            break;
+                        case 114:
+                            Rat rat = new Rat(new Position(j, i + 3), 'r', ConsoleColor.Red);
+                            _elements.Add(rat);
+                            break;
+                        case 115:
+                            Snake snake = new Snake(new Position(j, i + 3), 's', ConsoleColor.Green);
+                            _elements.Add(snake);
+                            break;
+                        case 116:
+                            Troll troll = new Troll(new Position(j, i + 3), 't', ConsoleColor.DarkCyan);
+                            _elements.Add(troll);
+                            break;
                     }
                 }
             }
