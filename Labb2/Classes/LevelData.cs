13dcc175f78bd2fa16969939e7d870bbdd5f4611
Jamesstahl98 <92599615+Jamesstahl98@users.noreply.github.com>
2024-10-02 15:53:51 +0200

    Update LevelData.cs
    Refactored Load method

--- Labb2/Classes/LevelData.cs
+++ Labb2/Classes/LevelData.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Globalization;
+using System.IO;
 using System.Security;
 
 public static class LevelData
@@ -12,48 +13,50 @@ public static class LevelData
         Player player = null;
         using (StreamReader reader = new StreamReader(fileName))
         {
-            string[] lines = File.ReadAllLines(fileName);
-            for (int i = 0; i < lines.Length; i++)
+            int lineCounter = 0;
+            while(!reader.EndOfStream)
             {
-                for (int j = 0; j <= lines[i].Length+1; j++)
+                string line = reader.ReadLine();
+                for (int i = 0; i < line.Length; i++)
                 {
-                    int charUnicode = reader.Read();
-                    switch(charUnicode)
+                    int charUnicode = line[i];
+                    switch (charUnicode)
                     {
                         case 33:
-                            Potion potion = new Potion(new Position(j, i + 3), '!', ConsoleColor.DarkGreen);
+                            Potion potion = new Potion(new Position(i, lineCounter + 3), '!', ConsoleColor.DarkGreen);
                             _elements.Add(potion);
                             break;
                         case 35:
-                            Wall wall = new Wall(new Position(j, i + 3), '#', ConsoleColor.Gray);
+                            Wall wall = new Wall(new Position(i, lineCounter + 3), '#', ConsoleColor.Gray);
                             _elements.Add(wall);
                             break;
                         case 64:
-                            player = new Player(new Position(j, i + 3), '@', ConsoleColor.Yellow);
+                            player = new Player(new Position(i, lineCounter + 3), '@', ConsoleColor.Yellow);
                             _elements.Add(player);
                             break;
                         case 97:
-                            Armor armor = new Armor(new Position(j, i + 3), 'a', ConsoleColor.DarkYellow);
+                            Armor armor = new Armor(new Position(i, lineCounter + 3), 'a', ConsoleColor.DarkYellow);
                             _elements.Add(armor);
                             break;
                         case 108:
-                            Sword sword = new Sword(new Position(j, i + 3), 'l', ConsoleColor.DarkYellow);
+                            Sword sword = new Sword(new Position(i, lineCounter + 3), 'l', ConsoleColor.DarkYellow);
                             _elements.Add(sword);
                             break;
                         case 114:
-                            Rat rat = new Rat(new Position(j, i + 3), 'r', ConsoleColor.Red);
+                            Rat rat = new Rat(new Position(i, lineCounter + 3), 'r', ConsoleColor.Red);
                             _elements.Add(rat);
                             break;
                         case 115:
-                            Snake snake = new Snake(new Position(j, i + 3), 's', ConsoleColor.Green);
+                            Snake snake = new Snake(new Position(i, lineCounter + 3), 's', ConsoleColor.Green);
                             _elements.Add(snake);
                             break;
                         case 116:
-                            Troll troll = new Troll(new Position(j, i + 3), 't', ConsoleColor.DarkCyan);
+                            Troll troll = new Troll(new Position(i, lineCounter + 3), 't', ConsoleColor.DarkCyan);
                             _elements.Add(troll);
                             break;
                     }
                 }
+                lineCounter++;
             }
         }
 
