74adf1de20122ef7f4943694a738c695d2ba07b3
Jamesstahl98 <Jamesstahl556@gmail.com>
2024-10-11 14:23:56 +0200

    Update Potion.cs
    Refactoring

--- Labb2/Classes/LevelElements/Items/Potion.cs
+++ Labb2/Classes/LevelElements/Items/Potion.cs
@@ -8,28 +8,27 @@
 
     public override void ElementContact(Creature element)
     {
-        if(element is Player)
+        element.HP += HPRestore;
+        if(element.HP>100)
         {
-            element.HP += HPRestore;
-            if(element.HP>100)
-            {
-                element.HP = 100;
-            }
-
-            UserInterface.PrintItemPickup(this);
-
-            RemoveElement();
+            element.HP = 100;
         }
+
+        UserInterface.PrintItemPickup(this);
+
+        RemoveElement();
     }
 
     public override void Update()
     {
-        Console.SetCursorPosition(Position.X, Position.Y);
-        Console.Write(" ");
-
         if (IsPlayerNearby())
         {
             Draw();
+        }
+        else
+        {
+            Console.SetCursorPosition(Position.X, Position.Y);
+            Console.Write(" ");
         }
     }
 
